0b2de7c048fed5ddb6ff5d0286b710ed3009c7c9
Angel Zhou <angel.zhou1016@gmail.com>
2022-09-03 17:18:17 -0400

    updates

---
+++ app.js
@@ -0,0 +1,15 @@
+const recipient = document.getElementById("recipient").value;
+const ordinalNumber = document.getElementById("ordinalNumber").value;
+const bodyPart1 = document.getElementById("bodyPart1").value;
+const adjective = document.getElementById("adjective").value;
+const livingThing = document.getElementById("livingThing").value;
+const place = document.getElementById("place").value;
+const feeling = document.getElementById("feeling").value;
+const verb = document.getElementById("verb").value;
+const bodyPart2 = document.getElementById("bodyPart2").value;
+const writer = document.getElementById("writer").value;
+const submit = document.getElementById("submit");
+
+function createStory () {
+  
+}
