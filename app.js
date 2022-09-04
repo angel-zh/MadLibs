db5e5d028596b3f6ecbdddfc106ae9984b60653a
Angel Zhou <angel.zhou1016@gmail.com>
2022-09-04 16:43:25 -0400

    updates

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const recipient = document.getElementById("recipient").value;
 const ordinalNumber = document.getElementById("ordinalNumber").value;
 const bodyPart1 = document.getElementById("bodyPart1").value;
-const adjective = document.getElementById("adjective").value;
+const adjective1 = document.getElementById("adjective1").value;
 const livingThing = document.getElementById("livingThing").value;
 const place = document.getElementById("place").value;
 const feeling = document.getElementById("feeling").value;
+const adjective2 = document.getElementById("adjective2").value;
 const verb = document.getElementById("verb").value;
 const bodyPart2 = document.getElementById("bodyPart2").value;
 const writer = document.getElementById("writer").value;
-const submit = document.getElementById("submit");
+const madLibsForm = document.getElementById("madLibsForm");
+const newStory = document.querySelector(".newStory");
 
-function createStory () {
-  
+function createStory() {
+  newStory.innerHTML = `Dear <span>${recipient}</span>,<br>
+I was in love the <span>${ordinalNumber}</span> time I saw your <span>${bodyPart1}</span>. You are <span>${adjective1}</span> and I am the luckiest <span>${livingThing}</span> in the entire <span>${place}</span>. I am so <span>${feeling}</span> that we met. There is simply no one as <span>${adjective2}</span> as you. I <span>${verb}</span> you more every single day and I can't wait to hold you in my <span>${bodyPart2}</span> again!<br>
+Love,<br>
+${writer}`;
 }
+
+madLibsForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  createStory();
+});
