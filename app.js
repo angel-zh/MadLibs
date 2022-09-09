53337ef0785fe000175a12dbc41f1700bf0487df
Angel Zhou <angel.zhou1016@gmail.com>
2022-09-09 12:44:51 -0400

    updates

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const recipient = document.getElementById("recipient");
-const ordinalNumber = document.getElementById("ordinalNumber");
-const bodyPart1 = document.getElementById("bodyPart1");
-const adjective1 = document.getElementById("adjective1");
-const livingThing = document.getElementById("livingThing");
-const place = document.getElementById("place");
-const feeling = document.getElementById("feeling");
-const adjective2 = document.getElementById("adjective2");
-const verb = document.getElementById("verb");
-const bodyPart2 = document.getElementById("bodyPart2");
-const writer = document.getElementById("writer");
+let recipient = document.getElementById("recipient");
+let ordinalNumber = document.getElementById("ordinalNumber");
+let bodyPart1 = document.getElementById("bodyPart1");
+let adjective1 = document.getElementById("adjective1");
+let livingThing = document.getElementById("livingThing");
+let place = document.getElementById("place");
+let feeling = document.getElementById("feeling");
+let adjective2 = document.getElementById("adjective2");
+let verb = document.getElementById("verb");
+let bodyPart2 = document.getElementById("bodyPart2");
+let writer = document.getElementById("writer");
 const madLibsForm = document.getElementById("madLibsForm");
 const storySection = document.getElementById("storySection");
 const formContainer = document.getElementById("formContainer");
@@ -18,7 +18,7 @@ function createStory() {
   const story = `<p>Dear <span>${recipient.value}</span>,</p>
 <p>I was in love the <span>${ordinalNumber.value}</span> time I saw your <span>${bodyPart1.value}</span>. You are <span>${adjective1.value}</span> and I am the luckiest <span>${livingThing.value}</span> in the entire <span>${place.value}</span>. I am so <span>${feeling.value}</span> that we met. There is simply no one as <span>${adjective2.value}</span> as you. I <span>${verb.value}</span> you more every single day and I can't wait to hold you in my <span>${bodyPart2.value}</span> again!</p>
 <p>Love,</p>
-<p><span>${writer.value}</span></p>`;
+<p><span">${writer.value}</span></p>`;
   storySection.innerHTML = story;
 }
 
@@ -30,3 +30,11 @@ madLibsForm.addEventListener("submit", function (event) {
   storySection.classList.remove("hide");
   restart.classList.remove("hide");
 });
+
+restart.addEventListener("click", function() {
+  formContainer.setAttribute("id", "formContainer");
+  madLibsForm.classList.remove("hide");
+  madLibsForm.reset();
+  storySection.classList.add("hide");
+  restart.classList.add("hide");
+})
